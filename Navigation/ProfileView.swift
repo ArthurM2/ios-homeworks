c2a4c464bab225936a53d275d4d9ee92ba710ed1
ArthurM2 <arthur.komarov@gmail.com>
2022-08-16 09:53:34 +0500

    обновил верстку под айфон 8

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -8,4 +8,23 @@ class ProfileView: UIView {
     @IBOutlet weak var userCityLabel: UILabel!
     @IBOutlet weak var userTextView: UITextView!
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setUp()
+    }
+    
+    private func setUp() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+    }
+    
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed(
+            "ProfileView", owner: self)?.first as? UIView else {
+                return UIView()
+                
+            }
+        return view
+    }
 }
