77acc1dd758af61d8011d689613c5a657fee3164
ArthurM2 <arthur.komarov@gmail.com>
2022-08-16 09:35:04 +0500

    added IBOutlets naming

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -5,4 +5,14 @@
 //  Created by a.malginov on 15.08.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var avatarUserImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userBirthdayLabel: UILabel!
+    @IBOutlet weak var userCityLabel: UILabel!
+    @IBOutlet weak var userTextView: UITextView!
+    
+}
