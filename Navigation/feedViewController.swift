62d949f68d67fcd06991d0d9d30b99cb9d5641cc
ArthurM2 <arthur.komarov@gmail.com>
2022-08-22 12:45:06 +0500

    добавлен таббар

---
+++ Navigation/feedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  feedViewController.swift
+//  Navigation
+//
+//  Created by a.malginov on 22.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class feedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemCyan
+    }
+}
---
+++ Navigation/profileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  profileViewController.swift
+//  Navigation
+//
+//  Created by a.malginov on 22.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class profileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+    }
+}
