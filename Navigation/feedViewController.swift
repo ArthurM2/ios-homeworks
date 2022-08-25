6c68e9e6cdc62d191d6844847b0776a8735d4fbb
ArthurM2 <arthur.komarov@gmail.com>
2022-08-25 11:27:06 +0500

    перенес в функцию

--- Navigation/feedViewController.swift
+++ Navigation/feedViewController.swift
@@ -13,17 +13,16 @@ class FeedViewController: UIViewController {
 
 
     func showButtonPost() {
-
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
         view.addSubview(button)
-
+        
         button.setTitle("Показать пост", for: .normal)
         button.setTitleColor(UIColor.black, for: .normal)
         button.backgroundColor = .white
         button.center = view.center
         button.addTarget(self, action: #selector(self.didTapButton), for: .touchUpInside)
-
     }
+
     @objc private func didTapButton() {
         let vc = PostViewController()
         vc.title = post.title
